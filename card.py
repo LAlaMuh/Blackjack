110653fe38924dd692064c4887e7f6fa9d13c8f5
LAlaMuh <al.ho.260@gmail.comgit log>
2022-05-27 19:01:29 +0200

    Fixing cards, apply them to __main__.

--- card.py
+++ card.py
@@ -1,33 +1,46 @@
 # TODO 1. create class Card to create Deck
 # creates every color of a given number as a Card class
 def deck_carder(*args, **kwargs):
-    if args == int
-        g = Card("green", args[0])
-        y = Card("yellow", args[0])
-        r = Card("red", args[0])
-        b = Card("black", args[0])
-    else:
+    try:
+        number = int(args[0])
+    except ValueError:
         if args[0] == "Ass":
             number = 11
         else:
             number = 10
-        g = Card("green", args[0], number)
-        y = Card("yellow", args[0], number)
-        r = Card("red", args[0], number)
-        b = Card("black", args[0], number)
+    g = Card("green", args[0], number)
+    y = Card("yellow", args[0], number)
+    r = Card("red", args[0], number)
+    b = Card("black", args[0], number)
 
     return g, y, r, b
 
 
 class Card:
     def __init__(self, *args, **kwargs):
-        self.name = f"{args[0]}{args[1]}"
+        self.name = f"{args[0]}_{args[1]}"
         self.color = args[0]
         self.number = args[-1]
 
-g1, y1, r1, b1 = deck_carder(1)
 
-print(f"{g1.name}:\n  value: {g1.color}\n  name: {g1.number}")
-print(f"{y1.name}:\n  value: {y1.color}\n  name: {y1.number}")
-print(f"{r1.name}:\n  value: {r1.color}\n  name: {r1.number}")
-print(f"{b1.name}:\n  value: {b1.color}\n  name: {b1.number}")
+card_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ass"]
+card_dict = {}
+def deck_bd_card_info():
+    for x in card_list:
+        g, y, r, b = deck_carder(x)
+        placeholdi = {
+            x:[
+            g.name, [g.color, g.number],
+            y.name, [y.color, y.number],
+            r.name, [r.color, r.number],
+            b.name, [b.color, b.number]
+        ]
+        }
+        card_dict[x] = placeholdi[x]
+    return card_dict
+
+#
+# print(f"{g.name}:\n  value: {g.color}\n  name: {g.number}")
+# print(f"{y.name}:\n  value: {y.color}\n  name: {y.number}")
+# print(f"{r.name}:\n  value: {r.color}\n  name: {r.number}")
+# print(f"{b.name}:\n  value: {b.color}\n  name: {b.number}")
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+from flask import Flask, render_template, abort, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from card import deck_bd_card_info, card_list
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+# Connecting to the db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+deck = deck_bd_card_info()
+answer = ""
+for x in card_list:
+    i = f"<h1>{x}:</h1>\n<h3>{deck[x]}</h3>\n\n"
+    answer = answer + i
+
+
+@app.route("/")
+def home():
+    return f"{answer}"
 
 # TODO 2. create class Deck with the class Card
 # TODO 3. start the game
@@ -6,6 +32,5 @@
 # TODO 6. add additional cards to your hand, if needed
 # TODO 7. flip cards and compare and declare the winner
 
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
